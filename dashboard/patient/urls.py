8aaad1147223b63b96cd1b007883d0908d31e0c1
suckzoo <suckzoo@sparcs.org>
2014-05-26 02:14:04 +0900

    progress

--- dashboard/patient/urls.py
+++ dashboard/patient/urls.py
@@ -1,19 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
-from django.contrib import admin
-admin.autodiscover()
-
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
-#print settings.STATIC_ROOT
+print settings.STATIC_ROOT
 
 urlpatterns = patterns('',
-    url(r'^report/', include('emergency.urls')),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': 'static'}),
-    url(r'^admin/',include(admin.site.urls)),
-    url(r'^patient/',include('patient.urls')),
+    url(r'^$', 'patient.views.overview'),
+    url(r'^question/', 'patient.views.question'),
     # Examples:
     # url(r'^$', 'dashboard.views.home', name='home'),
     # url(r'^dashboard/', include('dashboard.foo.urls')),
--- dashboard/patient/views.py
+++ dashboard/patient/views.py
@@ -1,1 +1,15 @@
 # Create your views here.
+
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template import Context
+from django.shortcuts import render
+from django.http import Http404
+#from models import 
+
+def overview(request):
+    return render(request, 'patient.html')
+
+def question(report):
+    return 1
+#    return render(request, 'question.html')
